d706f3aa684c706a847504a0002c93699b3e2264
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-11 20:36:36 +0300

    styled the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { FizziLogo } from "./FizziLogo";
 const Header = () => {
   return (
     <header className="-mb-28 flex justify-center py-4">
-      <FizziLogo />
+      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
     </header>
   );
 };
