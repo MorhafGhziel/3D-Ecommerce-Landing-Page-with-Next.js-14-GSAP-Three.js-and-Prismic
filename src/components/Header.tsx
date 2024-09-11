d7b4f15db432cb694d94b93559c2432048d04ea0
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-11 20:35:24 +0300

    added the logo to the page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { FizziLogo } from "./FizziLogo";
 
 const Header = () => {
-  return <header className="-mb-28 flex justify-center py-4"></header>;
+  return (
+    <header className="-mb-28 flex justify-center py-4">
+      <FizziLogo />
+    </header>
+  );
 };
 
 export default Header;
