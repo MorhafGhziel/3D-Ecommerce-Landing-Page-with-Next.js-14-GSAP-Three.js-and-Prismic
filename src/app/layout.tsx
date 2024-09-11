07795ae31a0e698833f98d1265fc25dbd111410f
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-11 20:14:40 +0300

    edited the alpino src

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { repositoryName } from "@/prismicio";
 import "./app.css";
 
 const alpino = localFont({
-  src: "../../public/fonts/Alpino-Varriable.woff2",
+  src: "../../public/fonts/Alpino-Variable.woff2",
   display: "swap",
   weight: "100 900",
   variable: "--font-alpino",
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={alpino.variable}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
