6d0511b6ca8aec922b0fe1a71e41b5f8e3d4da6e
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-12 08:42:54 +0300

    added more grids

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -21,14 +21,18 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
       <div className="grid">
-        <PrismicRichText field={slice.primary.heading} />
-        <PrismicRichText field={slice.primary.subheading} />
-        <PrismicRichText field={slice.primary.body} />
-        {slice.primary.button_text}
-        <PrismicNextLink field={slice.primary.button_link}></PrismicNextLink>
-        <PrismicNextImage field={slice.primary.cans_image} />
-        <PrismicRichText field={slice.primary.second_heading} />
-        <PrismicRichText field={slice.primary.second_body} />
+        <div className="grid">
+          <PrismicRichText field={slice.primary.heading} />
+          <PrismicRichText field={slice.primary.subheading} />
+          <PrismicRichText field={slice.primary.body} />
+          {slice.primary.button_text}
+          <PrismicNextLink field={slice.primary.button_link}></PrismicNextLink>
+        </div>
+        <div className="grid">
+          <PrismicNextImage field={slice.primary.cans_image} />
+          <PrismicRichText field={slice.primary.second_heading} />
+          <PrismicRichText field={slice.primary.second_body} />
+        </div>
       </div>
     </Bounded>
   );
