6591a99734eef21a6d2558410bc7dfea60d9adcb
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-12 08:39:27 +0300

    added grid around the texts

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -3,6 +3,7 @@ import { SliceComponentProps } from "@prismicio/react";
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
 import { PrismicNextImage } from "@prismicio/next";
+
 import { Bounded } from "@/components/Bounded";
 
 /**
@@ -19,14 +20,16 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.subheading} />
-      <PrismicRichText field={slice.primary.body} />
-      {slice.primary.button_text}
-      <PrismicNextLink field={slice.primary.button_link}></PrismicNextLink>
-      <PrismicNextImage field={slice.primary.cans_image} />
-      <PrismicRichText field={slice.primary.second_heading} />
-      <PrismicRichText field={slice.primary.second_body} />
+      <div className="grid">
+        <PrismicRichText field={slice.primary.heading} />
+        <PrismicRichText field={slice.primary.subheading} />
+        <PrismicRichText field={slice.primary.body} />
+        {slice.primary.button_text}
+        <PrismicNextLink field={slice.primary.button_link}></PrismicNextLink>
+        <PrismicNextImage field={slice.primary.cans_image} />
+        <PrismicRichText field={slice.primary.second_heading} />
+        <PrismicRichText field={slice.primary.second_body} />
+      </div>
     </Bounded>
   );
 };
