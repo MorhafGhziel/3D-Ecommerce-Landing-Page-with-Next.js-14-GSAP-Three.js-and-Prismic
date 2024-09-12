2a8271d6c270d770a6b07e862e0a5433d4a0ce38
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-12 09:10:10 +0300

    added the props types:

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { PrismicNextLink } from "@prismicio/next";
 import { LinkField } from "@prismicio/client";
 
-type Props = {};
+type Props = {
+  buttonLink: LinkField;
+  buttonText: string | null;
+  className?: string;
+};
 
 const Button = (props: Props) => {
   return <PrismicNextLink field={slice.primary.button_link}></PrismicNextLink>;
