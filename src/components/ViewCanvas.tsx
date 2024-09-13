14ea8f8af5e4ec831c4590f7e57c0b8e56c15a71
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-13 17:59:46 +0300

    added 3d to the box

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -7,11 +7,22 @@ type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas>
-      <mesh>
+    <Canvas
+      style={{
+        position: "fixed",
+        top: "0",
+        left: "50%",
+        transform: "translateX(-50%)",
+        overflow: "hidden",
+        pointerEvents: "none",
+        zIndex: "30",
+      }}
+    >
+      <mesh rotation={[0.5, 0.5, 0]}>
         <boxGeometry />
-        <meshStandardMaterial />
+        <meshStandardMaterial color={"hotpink"} />
       </mesh>
+      <ambientLight intensity={2} />
     </Canvas>
   );
 }
